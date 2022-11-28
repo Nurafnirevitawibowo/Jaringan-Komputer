0cd4ad45c54574709ebc8c762cc8bded366640ed
Nurafnirevitawibowo <91048061+Nurafnirevitawibowo@users.noreply.github.com>
2022-11-28 09:02:05 +0700

    Create server.py

---
+++ Modul 11/server.py
@@ -0,0 +1,15 @@
+(base) C:\Users\Afni>cd ..
+
+(base) C:\Users>cd ..
+
+(base) C:\>cd Pyton2
+
+(base) C:\Pyton2>server.py
+
+(base) C:\Pyton2>server.py
+Host: localhost
+Port: 0001
+New connection at ID 0 ('127.0.0.1', 52256)
+New connection at ID 1 ('127.0.0.1', 52302)
+ID 0: hola
+ID 1: kl
